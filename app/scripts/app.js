581575d62a29eea6adccbe4f939269425ad7f711
RaduM <radu.micu@digitaslbi.com>
2016-10-03 09:42:59 +0100

    Refactor some of the JS code and simplify html structure

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,6 +19,16 @@ class App {
 	loadPortfolioItems() {
 		this.flickr.getPhotos().then(data => Flickr.getPhotoSourceURLs(data))
 		.then((data) => {
+			const portfolio = this.document.querySelectorAll('.Portfolio')[0];
+			const portfolioItemEl = this.document.querySelectorAll('.Portfolio__item')[0];
+			let portfolioItemElClone;
+
+			for (let i = 0; i < 5; i += 1) {
+				portfolioItemElClone = portfolioItemEl.cloneNode(true);
+
+				portfolio.appendChild(portfolioItemElClone);
+			}
+
 			this.document.querySelectorAll('.Portfolio__item').forEach((media, index) => {
 				const pItemImg = media.querySelector('img');
 				const pItemTitle = media.querySelector('.Portfolio__title h2');
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,17 @@
+/* global document */
+
 // Global imports
 import 'babel-polyfill';
 import 'whatwg-fetch';
-
-import FastClick from 'fastclick';
-
+import fastClick from 'fastclick';
 import App from './app';
-
-const app = new App();
 
 // Eliminates the 300ms delay between a physical tap
 // and the firing of a click event on mobile browsers
 // https://github.com/ftlabs/fastclick
-FastClick.attach(document.body);
+fastClick(document.body);
+
+const app = new App();
 
 app.sayHi('RaduM');
 app.loadPortfolioItems();
